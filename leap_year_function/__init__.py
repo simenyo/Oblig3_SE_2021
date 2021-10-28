a76dc3a8e31ca85c2f67455c7e0872109a0718c9
Simen Jacobsen Øygard <simen.90@gmail.com>
2021-10-28 14:35:32 +0200

    Initial commit

---
+++ leap_year_function/__init__.py
@@ -0,0 +1,10 @@
+def bool_is_leap_year(year) -> bool:
+    """ Returns true/false if year is a leap year """
+    if year % 400 == 0:
+        return True
+    elif year % 100 == 0:
+        return False
+    elif year % 4 == 0:
+        return True
+    else:
+        return False
---
+++ tests/test_bool_is_leap_year.py
@@ -0,0 +1,31 @@
+from leap_year_function import bool_is_leap_year
+
+
+def test_is_leap_year_when_divisible_by_4_not_100():
+    """ Divisible by 4, and not 100"""
+    assert bool_is_leap_year(88) is True
+
+
+def test_leap_year_is_divisible_by_400():
+    """If a year is divisible by 400, it's a leap year"""
+    assert bool_is_leap_year(400) is True
+
+
+def test_is_not_leap_year_when_not_divisible_by_four():
+    """"""
+    assert bool_is_leap_year(5) is False
+
+
+def test_is_not_leap_year_when_divisible_by_100_not_400():
+    """ """
+    assert bool_is_leap_year(200) is False
+
+
+def test_1700_not_leap_year():
+    """"""
+    assert bool_is_leap_year(1700) is False
+
+
+def test_2000_is_a_leap_year():
+    """"""
+    assert bool_is_leap_year(2000) is True
